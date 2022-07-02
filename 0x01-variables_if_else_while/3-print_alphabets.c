c22f1ac1094d67a50acbb61ef3d9b4e008ee69ac
peaceik <igwudup@gmail.com>
2022-07-01 20:34:41 -0700

    kite

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+*main -> assign a random number to the variabe n each time it is executed and print the la*st digit of the number stored in the variable n
+*Return : always 0
+*/
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <=122; <h++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /* this is an ascii code for new line*/
+
+	return (0);
+}
