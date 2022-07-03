15eca9fdbf07f4c34e86c2abe7fdc85d1da40b00
peaceik <igwudup@gmail.com>
2022-07-03 11:03:19 -0700

    kite

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,14 +6,14 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- *    
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
+{	
 	int n, s;
-
-        srand(time(0));
+	
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/*your codes goes there */
 	s = n % 10;
@@ -26,6 +26,8 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, s);				}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);	        }
+		printf("Last digit of %d is %d and is less than 6
+			       	and not 0\n", n, s);
+	}
 	return (0);
 }
