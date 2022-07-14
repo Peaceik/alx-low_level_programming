2ee30c4693ba88f6b1525330a57bd65d9c9762d9
peaceik <igwudup@gmail.com>
2022-07-14 14:40:21 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		m =n * -1;
 	}
 	else
 	{
-		m = n;
+		m =n;
 	}
 	d = m;
 	count = 1;
@@ -24,7 +24,7 @@ void print_number(int n)
 	while (d > 9)
 	{
 		d /= 10;
-		count *= 10;
+		count * = 10;
 	}
 	for (; count >= 1; count /= 10)
 	{
